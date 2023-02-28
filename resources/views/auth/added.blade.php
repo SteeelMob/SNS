577a1aa4be409c11bf0d2bc11269fda2ee85132e
ganechu <ganechu.mouse@gmail.com>
2023-03-01 06:07:34 +0900

    test

--- resources/views/auth/added.blade.php
+++ resources/views/auth/added.blade.php
@@ -4,12 +4,14 @@
 
 <div id="clear">
   <!--ファサードのAuthはインスタンスやidを取得するためのもの-->
-  <p> <!--{{ Auth::user() -> username }}-->OOさん</p> 
+  <p> <!--{{ Auth::user() -> username }}-->OOさん</p>
   <p>ようこそ！AtlasSNSへ！</p>
   <p>ユーザー登録が完了しました。</p>
   <p>早速ログインをしてみましょう。</p>
 
   <p class="btn"><a href="/login">ログイン画面へ</a></p>
 </div>
+
+<!--テスト-->
 
 @endsection
