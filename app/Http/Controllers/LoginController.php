c959081f2472ac1cfe81221ea1410cb6cfafd5bf
ganechu <ganechu.mouse@gmail.com>
2023-03-04 21:42:58 +0900

    posts/indexに移るためのコントローラーを記述

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+//Authファサードを読み込ませるため
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     //
+    //認証の試行を処理らしい...
+    public function login(Request $request)
+    {
+        $user_info = $request ->validate([
+            'email' => ['required','email'],
+            'password' => ['required']
+        ]);
+
+        //ログイン成功時
+        if (Auth::attempt($user_info)){
+            $request ->session() ->regenerate();
+            return redirect() ->route('posts/index');
+        }
+
+        //ログイン失敗
+        return redirect() ->back();
+    }
 }
